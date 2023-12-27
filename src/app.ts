5b5e5069b6a066f4182b4f17c4d6a4ebb6fc4aae
TresorRw <alaintresorcyusa683@gmail.com>
2023-12-27 23:58:00 +0200

    Connects my app to DB

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import express, { Application, Request, Response } from "express";
 import { config } from "dotenv";
+import { connectDB } from "./utils";
 config();
 
 const app: Application = express();
 const PORT = process.env.PORT || (3000 as number);
+app.use(express.json());
+
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    throw new Error(`Database connection error: ${error.message}`);
+  });
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello, World!" });
