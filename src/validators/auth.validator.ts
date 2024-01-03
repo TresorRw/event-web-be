3b06b46fbd1c2a48b67ccf74ad9160c64e75a78a
TresorRw <alaintresorcyusa683@gmail.com>
2024-01-03 13:29:12 +0200

    Auth request body validators

---
+++ src/validators/auth.validator.ts
@@ -0,0 +1,19 @@
+import * as v from "valibot";
+
+export const RegsiterSchema = v.object({
+  email: v.string("Your email should be string", [
+    v.email("Your email should be a valid email address"),
+  ]),
+  password: v.string("Your password should be string", [
+    v.minLength(8, "Your password should be at least 8 characters"),
+    v.regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+      "Your password should contain at least one lowercase letter, one uppercase letter, one digit and one special character",
+    ),
+  ]),
+  displayName: v.string("Your name should be string", [
+    v.minLength(3, "Your name should be at least 3 characters"),
+  ]),
+});
+
+export const LoginSchema = v.pick(RegsiterSchema, ["email", "password"]);
