4b2bf8bc9fe9aba66272002631156e2029a117e0
TresorRw <alaintresorcyusa683@gmail.com>
2024-01-03 15:46:53 +0200

    Added jwt util for signing/verifying token

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export * from "./db.util";
 export * from "./password.util";
 export * from "./Error.util";
+export * from "./token.util";
---
+++ src/utils/token.util.ts
@@ -0,0 +1,26 @@
+import { config } from "dotenv";
+import { sign, verify } from "jsonwebtoken";
+config();
+
+const secret = process.env.JWT_SECRET;
+if (!secret) {
+  throw new Error("JWT_SECRET is not defined");
+}
+
+export const generateToken = (payload: {
+  _id: string;
+  displayName: string;
+  email: string;
+}) => {
+  const token: string = sign(payload, secret, { expiresIn: "7d" });
+  return token;
+};
+
+export const verifyToken = (token: string) => {
+  try {
+    const payload = verify(token, secret);
+    return payload;
+  } catch (error) {
+    throw new Error("Invalid or expired token");
+  }
+};
