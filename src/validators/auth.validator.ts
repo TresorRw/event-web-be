61418c1bfcd7a3426c6e853394b246fd74a977ee
TresorRw <alaintresorcyusa683@gmail.com>
2024-01-06 18:11:52 +0200

    Adde role validation on request payload

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -1,9 +1,11 @@
 import * as v from "valibot";
+import { UserRole } from "../models";
 
 export const RegisterSchema = v.object({
   email: v.string("Your email should be string", [
     v.email("Your email should be a valid email address"),
   ]),
+  role: v.enum_(UserRole, "Your role should either be organizer or attendee"),
   password: v.string("Your password should be string", [
     v.minLength(8, "Your password should be at least 8 characters"),
     v.regex(
