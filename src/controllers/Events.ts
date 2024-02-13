60eea399c35361185d83582e6692e50354ff2654
TresorRw <alaintresorcyusa683@gmail.com>
2024-02-13 16:57:23 +0200

    Event controller to store user ObjectId

--- src/controllers/Events.ts
+++ src/controllers/Events.ts
@@ -1,10 +1,14 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { EventSchema } from "../validators";
 import { safeParse } from "valibot";
 import { validationMessages } from "../utils";
+import { CustomReq } from "../middlewares";
+import { Event } from "../models";
 
-export async function CreateEvent(req: Request, res: Response) {
+export async function CreateEvent(req: CustomReq, res: Response) {
   const body = req.body;
+  const user = req.user;
+  // console.log(user);
   const result = safeParse(EventSchema, body);
   if (result.issues) {
     return res.status(400).json({
@@ -13,11 +17,18 @@ export async function CreateEvent(req: Request, res: Response) {
       errors: validationMessages(result.issues),
     });
   }
+
+  // Save to database
   if (result.success) {
+    const event = await Event.create({
+      ...result.output,
+      organizer: user?._id,
+    });
+
     return res.status(201).json({
       statusCode: 201,
       message: "Event created successfully",
-      data: result.output,
+      data: event,
     });
   }
 }
