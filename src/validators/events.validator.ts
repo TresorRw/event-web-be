7a69338453750842bf08aef35ef8d90cc68677ee
TresorRw <alaintresorcyusa683@gmail.com>
2024-02-13 16:56:32 +0200

    Refactor: attributes for event

--- src/validators/events.validator.ts
+++ src/validators/events.validator.ts
@@ -13,8 +13,8 @@ export const EventSchema = v.object({
     "Event category should be one of the following: " +
       Object.values(EventCategories).join(", "),
   ),
-  startDateTime: v.string("Event start date should be a valid date"),
-  endDateTime: v.string("Event end date should be a valid date"),
+  startDateTime: v.string("Event start date and time should be a valid date"),
+  endDateTime: v.string("Event end date and time should be a valid date"),
   location: v.string("Event location should be string"),
   price: v.number("Event price should be a number"),
 });
