07acbe1099973e5f18ac71114ca56b656bc5c24a
TresorRw <alaintresorcyusa683@gmail.com>
2024-02-15 01:03:48 +0200

    Added update and delete functionality

--- src/controllers/Events.ts
+++ src/controllers/Events.ts
@@ -75,3 +75,61 @@ export const GetEvent = async (req: CustomReq, res: Response) => {
     return errorHandler(res, 500, `Something went wrong, try again!`);
   }
 };
+
+export const UpdateEvent = async (req: CustomReq, res: Response) => {
+  const eventId = req.params.eventId;
+  if (!isValidObjectId(eventId)) {
+    return errorHandler(res, 400, "Invalid event ID, provide correct an ID");
+  }
+  const body = req.body;
+  const event = await Event.findOne({ _id: eventId, organizer: req.user?._id });
+  if (!event) {
+    return errorHandler(res, 404, `Event with ID: ${eventId} is not found`);
+  }
+  const result = safeParse(EventSchema, body);
+  if (result.issues) {
+    return res.status(400).json({
+      statusCode: 400,
+      message: "Invalid body",
+      errors: validationMessages(result.issues),
+    });
+  }
+  if (result.success) {
+    try {
+      const updatedEvent = await Event.findByIdAndUpdate(
+        eventId,
+        {
+          ...result.output,
+        },
+        { new: true },
+      );
+      return res.status(200).json({
+        statusCode: 200,
+        message: "Event updated successfully",
+        data: updatedEvent,
+      });
+    } catch (error: any) {
+      return errorHandler(res, 500, `Something went wrong, try again!`);
+    }
+  }
+};
+
+export const DeleteEvent = async (req: CustomReq, res: Response) => {
+  const eventId = req.params.eventId;
+  if (!isValidObjectId(eventId)) {
+    return errorHandler(res, 400, "Invalid event ID, provide correct an ID");
+  }
+  const event = await Event.findOne({ _id: eventId, organizer: req.user?._id });
+  if (!event) {
+    return errorHandler(res, 404, `Event with ID: ${eventId} is not found`);
+  }
+  try {
+    await Event.findByIdAndDelete(eventId);
+    return res.status(200).json({
+      statusCode: 200,
+      message: "Event deleted successfully",
+    });
+  } catch (error: any) {
+    return errorHandler(res, 500, `Something went wrong, try again!`);
+  }
+};
--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,6 +1,12 @@
 import { Router } from "express";
 import { CheckAndVerifyAuthHeader } from "../middlewares/auth.middleware";
-import { CreateEvent, GetEvent, GetEvents } from "../controllers/Events";
+import {
+  CreateEvent,
+  DeleteEvent,
+  GetEvent,
+  GetEvents,
+  UpdateEvent,
+} from "../controllers/Events";
 import { forOrganizersOnly } from "../middlewares";
 
 const EventRouter = Router();
@@ -8,5 +14,16 @@ const EventRouter = Router();
 EventRouter.post("/", CheckAndVerifyAuthHeader, forOrganizersOnly, CreateEvent);
 EventRouter.get("/", CheckAndVerifyAuthHeader, GetEvents);
 EventRouter.get("/:eventId", CheckAndVerifyAuthHeader, GetEvent);
-
+EventRouter.patch(
+  "/:eventId",
+  CheckAndVerifyAuthHeader,
+  forOrganizersOnly,
+  UpdateEvent,
+);
+EventRouter.delete(
+  "/:eventId",
+  CheckAndVerifyAuthHeader,
+  forOrganizersOnly,
+  DeleteEvent,
+);
 export default EventRouter;
