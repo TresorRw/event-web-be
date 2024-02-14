1b96d7886f8f4737b70d87016456e9f313d6309d
TresorRw <alaintresorcyusa683@gmail.com>
2024-02-14 23:31:40 +0200

    Refactor: added error handling incase error during saving event

--- src/controllers/Events.ts
+++ src/controllers/Events.ts
@@ -8,7 +8,6 @@ import { Event } from "../models";
 export async function CreateEvent(req: CustomReq, res: Response) {
   const body = req.body;
   const user = req.user;
-  // console.log(user);
   const result = safeParse(EventSchema, body);
   if (result.issues) {
     return res.status(400).json({
@@ -20,15 +19,22 @@ export async function CreateEvent(req: CustomReq, res: Response) {
 
   // Save to database
   if (result.success) {
-    const event = await Event.create({
-      ...result.output,
-      organizer: user?._id,
-    });
+    try {
+      const event = await Event.create({
+        ...result.output,
+        organizer: user?._id,
+      });
 
-    return res.status(201).json({
-      statusCode: 201,
-      message: "Event created successfully",
-      data: event,
-    });
+      return res.status(201).json({
+        statusCode: 201,
+        message: "Event created successfully",
+        data: event,
+      });
+    } catch (error: any) {
+      return res.status(500).json({
+        statusCode: 500,
+        message: `Something went wrong: ${error.message}`,
+      });
+    }
   }
 }
