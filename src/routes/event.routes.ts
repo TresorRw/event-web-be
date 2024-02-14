586a4ba656075a2d2e152de2fc6d72fba5fb461d
TresorRw <alaintresorcyusa683@gmail.com>
2024-02-14 23:34:34 +0200

    Added forOrganizersOnly on createEvent

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { CheckAndVerifyAuthHeader } from "../middlewares/auth.middleware";
 import { CreateEvent } from "../controllers/Events";
+import { forOrganizersOnly } from "../middlewares";
 
 const EventRouter = Router();
 
-EventRouter.post("/", CheckAndVerifyAuthHeader, CreateEvent);
+EventRouter.post("/", CheckAndVerifyAuthHeader, forOrganizersOnly, CreateEvent);
 
 export default EventRouter;
