548d4c5a898716ed03f4ec16eaa10dc43bab4875
TresorRw <alaintresorcyusa683@gmail.com>
2024-03-23 13:38:06 +0200

    Swagger doc for auth

---
+++ src/docs/auth.docs.ts
@@ -0,0 +1,83 @@
+/**
+ * @swagger
+ * /api/auth/signup:
+ *   post:
+ *     summary: Create a user account
+ *     tags: [Auth]
+ *     description: Route for user to register themselves
+ *     security: []
+ *     requestBody:
+ *       description: User information
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "your-email@gmail.com"
+ *               role:
+ *                 type: string
+ *                 example: attendee
+ *               password:
+ *                 type: string
+ *                 example: Strong@123
+ *               displayName:
+ *                 type: string
+ *                 example: User
+ *     responses:
+ *       '201':
+ *         description: User is successfull registred
+ *       '400':
+ *         description: Invalid request body or missing params
+ *       '409':
+ *         description: Email is already taken
+ *       '500':
+ *         description: Internal server error
+ *
+ * /api/auth/signin:
+ *   post:
+ *     summary: Log into user account
+ *     tags: [Auth]
+ *     description: Route for user to log into their accoutn
+ *     security: []
+ *     requestBody:
+ *       description: User information
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: "your-email@gmail.com"
+ *               password:
+ *                 type: string
+ *                 example: Strong@123
+ *     responses:
+ *       '200':
+ *         description: User is successfull logged in
+ *       '400':
+ *         description: Invalid request body or missing params
+ *       '404':
+ *         description: User can not be found
+ *       '406':
+ *         description: User exists but incorrect password
+ *       '500':
+ *         description: Internal server error
+ *
+ * /api/auth/users:
+ *   get:
+ *     summary: View all users (This will be restricted to admin only)
+ *     tags: [Auth]
+ *     description: Route for users
+ *     security: []
+ *     responses:
+ *       '200':
+ *         description: Array of users
+ *       '500':
+ *         description: Internal server error
+ *
+ */
